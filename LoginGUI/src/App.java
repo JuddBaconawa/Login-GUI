77b9991f3fd2a891c56d19edf293ddb2cd0f60b0
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-02-19 12:51:31 -0500

    second commit
    -Jframe parts added
    -username label and textbox + password label and password box
    -actionPerformed method added to handle the log in button function after press
    -imports added

--- LoginGUI/src/App.java
+++ LoginGUI/src/App.java
@@ -2,33 +2,89 @@
 
 
 //imports
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 //main class
-public class App {
+public class App implements ActionListener {
+
+    private static JLabel userLabel;
+    private static JTextField userText;
+    private static JLabel passwordLabel;
+    private static JPasswordField passwordText; //JPasswordField to ensure password is private
+    private static JButton button;
+    private static JLabel success;
 
     //main method
     public static void main(String[] args) throws Exception {
 
-        JFrame frame = new JFrame();
-        frame.setSize(100, 200);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-
+        //sets the windows
         JPanel panel = new JPanel();
+        JFrame frame = new JFrame();
+        frame.setSize(350, 200); //frame size
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         frame.add(panel);
 
         panel.setLayout(null);
 
-        JLabel label = new JLabel();
-        label.setBounds(10, 20, 80, 25);
-        panel.add(label);
+        //username label
+        userLabel = new JLabel("Username");
+        userLabel.setBounds(10, 20, 80, 25);
+        panel.add(userLabel);
 
-        JTextField userText = new JTextField();
+        //username input box
+        userText = new JTextField();
+        userText.setBounds(100, 20, 165, 25);
+        panel.add(userText);
+
+        //password label
+        passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 50, 80, 25);
+        panel.add(passwordLabel);
+
+        //password input box
+        passwordText = new JPasswordField();
+        passwordText.setBounds(100, 50, 165, 25);
+        panel.add(passwordText);
+
+        button = new JButton("Login");
+        button.setBounds(125, 80, 80, 25);
+        button.addActionListener(new App());
+        panel.add(button);
+
+        success = new JLabel("");
+        success.setBounds(10, 110, 300, 25);
+        panel.add(success);
+        success.setText("");
 
 
+        frame.setVisible(true);
+
+
+    }
+
+    //acction event for when the "Login" button is clicked
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String user = userText.getText();
+        String password = passwordText.getText();
+        System.out.println(user + ", " + password);
+
+        //if statement to check the hardcoded credentials
+        if(user.equals("admin") && password.equals("password01")) {
+            success.setText("Login successful!");
+        } else {
+            success.setText("Invalid credentials!");
+            //System.out.println("Invalid Credentials!");
+            userText.setText("/*Invalid username*/");
+            passwordText.setText("Invalid password");
+        }
     }
 }
